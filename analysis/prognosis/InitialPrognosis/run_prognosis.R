4c8ee8852662f866d894fa701b5a26a20a2187c4
Tazi <taziy@lski2678.mskcc.org>
2019-05-13 12:03:39 -0400

    change

---
+++ analysis/prognosis/InitialPrognosis/run_prognosis.R
@@ -0,0 +1,157 @@
+library(glmnet)
+library(doMC)
+library(survival)
+library(data.table)
+library(mltools)
+library(CoxBoost)
+library(randomForestSRC)
+library(CoxHD)
+### All the predictors
+###---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+predictorGLM <- function(designTrain, designTest, responseTrain, alpha, ninternalfolds=10) {
+    # alpha=1 --> l1 penalty
+    # alpha=0 --> l2 penalty
+    # alpha=1/2 --> elastic net
+    set.seed(17)
+    # Train
+    cvfit = cv.glmnet(designTrain, responseTrain, family="cox", alpha=alpha, nfolds=ninternalfolds, grouped=TRUE)
+    # Predict
+    risk.predict = predict(cvfit, newx=designTest, s="lambda.min", type="response")
+    risk.predict = as.vector(risk.predict[,1])
+
+    return(risk.predict)
+}    
+
+predictorBoost<-function(designTrain, designTest, responseTrain){
+  set.seed(17)
+  cvfit<-CoxBoost(time=responseTrain[,1],
+                  status=responseTrain[,2],
+                  x=designTrain)
+  
+  risk.predict<-predict(cvfit,designTest,newtime=responseTest[,1],newstatus=responseTest[,2],type='lp')
+  
+  return(as.vector(risk.predict))
+}
+predictorRF <- function(designTrain, designTest, responseTrain, ntree=ntree, importance="none") {
+    set.seed(17)
+    # Train
+    cvfit = rfsrc(Surv(time, status) ~ ., data=data.frame(designTrain,responseTrain), ntree=ntree, importance=importance)
+    
+    # Predict
+    risk.predict = predict(cvfit, data.frame(designTest), importance=importance)$predicted
+    
+    return(risk.predict)
+} 
+predictorAIC <- function(designTrain, designTest, responseTrain) {
+    set.seed(17)
+    # Train
+    c <- coxph(Surv(time, status) ~ ., data=data.frame(designTrain,responseTrain))
+    scopeStep <- as.formula(paste("Surv(time,status) ~", paste(colnames(designTrain), collapse="+")))
+    cvfit<-step(c, scope=scopeStep, k = 2, trace=0)
+    # Predict
+    risk.predict = predict(cvfit, data.frame(designTest))
+    
+    return(risk.predict)
+}
+predictorRFX <- function(designTrain, designTest, responseTrain, max.iter = 500) {
+    set.seed(17)
+    # Train
+    cvfit = CoxRFX(data.frame(designTrain), Surv(time=responseTrain[,1],event =responseTrain[,2]) , max.iter =max.iter)
+    cvfit$Z <- NULL
+    # Predict
+    risk.predict<-predict(cvfit,data.frame(designTest))
+    
+    return(risk.predict)
+}
+predictorCPSS <- function(designTrain, designTest, responseTrain, bootstrap.samples = 50, nlambda = 100, mc.cores = 1) {
+    set.seed(17)
+    # Train
+    cvfit = CoxCPSSInteractions(data.frame(designTrain), Surv(time=responseTrain[,1],event =responseTrain[,2]), bootstrap.samples = bootstrap.samples, nlambda = nlambda, mc.cores = mc.cores)
+    # Predict
+    risk.predict = predict(cvfit, data.frame(designTest))
+    
+    return(risk.predict)
+}
+
+
+###---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+### Cross Validation Function
+###---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+runCV <- function(mypredictor, response, design, nfolds=nfolds, nrepeats=nrepeats, seed=seed, mc.cores=mc.cores,alpha=alpha,use_alpha=FALSE,use_ntree=FALSE,ntree, ...) {
+    # function that run "mypredictor" on a CV setting
+    #
+    # output a list of size the number of CV experiments (eg 50) (= nfolds x nrepeats)
+    
+    # "ref" contains the responses of the fold test set
+
+    #  random number generator seed
+    set.seed(seed)
+
+    # Make folds
+    n = nrow(design)
+    folds <- list()
+    for (i in seq(nrepeats)) {
+        folds <- c(folds,split(sample(seq(n)), rep(1:nfolds, length = n)))
+    }
+    nexp = length(folds) # the total number CV of experiments
+
+    # Parallel CV
+    print("start CV")
+    rescv = mclapply(seq(nexp),
+                   FUN=function(iexp) {
+                       cat(".")
+                       vTrain = design[-folds[[iexp]],,drop=F]
+                       vTest = design[folds[[iexp]],,drop=F]
+                       lTrain = response[-folds[[iexp]],]
+                       lTest = response[folds[[iexp]],]
+                       # Train and Predcit
+                       #predict.test = ifelse(use_alpha==TRUE,mypredictor(designTrain=vTrain, designTest=vTest, response=lTrain,alpha=alpha, ...),mypredictor(designTrain=vTrain, designTest=vTest, response=lTrain, ...))
+                       if(use_alpha){
+                           predict.test = mypredictor(designTrain=vTrain, designTest=vTest, response=lTrain,alpha=alpha, ...)
+                       }else if(use_ntree) {
+                           predict.test = mypredictor(designTrain=vTrain, designTest=vTest, response=lTrain,ntree=ntree, ...)
+                       }else{
+                           predict.test = mypredictor(designTrain=vTrain, designTest=vTest, response=lTrain, ...)
+                       }
+                       #predict.test = mypredictor(designTrain=vTrain, designTest=vTest, response=lTrain,alpha=alpha, ...)
+                       # Evaluate CI on the test
+                       ci.test = suppressWarnings(survConcordance(Surv(time,status) ~ predict.test, as.data.frame(lTest)))
+                       return(as.vector(ci.test$concordance))
+                   },
+                   mc.cores=mc.cores
+                   )
+
+    return(unlist(rescv))
+
+}
+###---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+### Launch prognosis
+###---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+launch_prognosis <- function(x,y,predictors,str_predictors,l_alpha,l_ntree,nfolds=5,nrepeats=5,mc.cores=25,seed=17){
+    set.seed(seed)
+    i<-0
+    j<-0
+    k<-0
+    colnames(y) = c("time","status")
+    table_w_c_i <- c()
+    for(predictor in predictors){
+        use_alpha<-ifelse(identical(predictorGLM,predictor),TRUE,FALSE)
+        use_ntree<-ifelse(identical(predictorRF,predictor),TRUE,FALSE)
+        i <- i+1
+        j <-ifelse(use_alpha,j+1,j)
+        k <-ifelse(use_ntree,k+1,k)
+        alpha <- l_alpha[j]
+        ntree <-l_ntree[k]
+        tmp <- runCV(mypredictor=predictor,
+              response=y, design=x,
+              nfolds=nfolds, nrepeats=nrepeats, seed=233,use_alpha=use_alpha,alpha=alpha,use_ntree=use_ntree,ntree=ntree, mc.cores=mc.cores)
+        table_w_c_i <- cbind(table_w_c_i,tmp)
+        colnames(table_w_c_i) [i] <-paste(str_predictors[i],ifelse(use_alpha,alpha,
+                                                                                       ifelse(use_ntree,ntree,"")),sep="_")
+        }
+    
+    return (table_w_c_i)
+    
+    }
+###---------------------------------------------------------------------------------------------------------------------------------------------------------------------
