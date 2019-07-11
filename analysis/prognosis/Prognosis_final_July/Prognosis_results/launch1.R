bc872289e27645ab38fc7b82e613ff65560aa218
Tazi <taziy@lski2678.mskcc.org>
2019-07-11 19:45:25 -0400

    changes

---
+++ analysis/prognosis/Prognosis_final_July/Prognosis_results/launch1.R
@@ -0,0 +1,140 @@
+library(glmnet)
+library(doMC)
+library(survival)
+library(data.table)
+library(mltools)
+library(CoxBoost)
+library(randomForestSRC)
+library(CoxHD)
+source('../../tools_prognosis/run_prognosis.R')
+
+df_final <- read.table("../data_frame_final_prognosis.tsv")
+
+
+eln <- c(1)
+comp <- c(164:178)
+age <- c(163)
+gen <- c(2:85)
+cyto <- c(86:154)
+clin <- c(155:161)
+demo <- c(162:163)
+demo_without_age <-c(162)
+           
+                          
+                          
+eln_comp <- c(eln,comp)
+eln_age <- c(eln,age)
+eln_gen <- c(eln,gen)
+eln_cyto <- c(eln,cyto)
+eln_clin <- c(eln,clin)
+eln_demo <- c(eln,demo)
+eln_demo_without_age <- c(eln,demo_without_age)
+
+# USEFUL FOR ELN COMPARISON
+# with comp
+eln_comp_age <- c(eln_comp,age)
+eln_comp_gen <- c(eln_comp,gen)
+eln_comp_cyto <- c(eln_comp,cyto)
+eln_comp_clin <- c(eln_comp,clin)
+eln_comp_demo <- c(eln_comp,demo)
+eln_comp_demo_without_age <- c(eln_comp,demo_without_age)
+
+eln_comp_age_gen <- c(eln_comp_age,gen)
+eln_comp_age_cyto <- c(eln_comp_age,cyto)
+eln_comp_age_clin <- c(eln_comp_age,clin)
+
+eln_comp_gen_cyto <- c(eln_comp_gen,cyto)
+eln_comp_gen_clin <- c(eln_comp_gen,clin)
+eln_comp_gen_demo <- c(eln_comp_gen,demo)
+eln_comp_gen_demo_without_age <- c(eln_comp_gen,demo_without_age)
+
+eln_comp_cyto_clin <- c(eln_comp_cyto,clin)
+eln_comp_cyto_demo <- c(eln_comp_cyto,demo)
+eln_comp_cyto_demo_without_age <- c(eln_comp_cyto,demo_without_age)
+
+eln_comp_clin_demo <- c(eln_comp_clin,demo)
+eln_comp_clin_demo_without_age <- c(eln_comp_clin,demo_without_age)
+
+eln_comp_age_gen_cyto <- c(eln_comp_age_gen,cyto)
+eln_comp_age_gen_clin <- c(eln_comp_age_gen,clin)
+eln_comp_age_gen_demo <- c(eln_comp_age_gen,demo)
+eln_comp_age_gen_demo_without_age <- c(eln_comp_age_gen,demo_without_age)
+
+eln_comp_gen_cyto_clin_demo <- c(eln_comp_gen_cyto,clin,demo)
+
+# without comp
+
+eln_age_gen <- c(eln_age,gen)
+eln_age_cyto <- c(eln_age,cyto)
+eln_age_clin <- c(eln_age,clin)
+
+eln_gen_cyto <- c(eln_gen,cyto)
+eln_gen_clin <- c(eln_gen,clin)
+eln_gen_demo <- c(eln_gen,demo)
+eln_gen_demo_without_age <- c(eln_gen,demo_without_age)
+
+eln_cyto_clin <- c(eln_cyto,clin)
+eln_cyto_demo <- c(eln_cyto,demo)
+eln_cyto_demo_without_age <- c(eln_cyto,demo_without_age)
+
+eln_clin_demo <- c(eln_clin,demo)
+eln_clin_demo_without_age <- c(eln_clin,demo_without_age)
+
+eln_age_gen_cyto <- c(eln_age_gen,cyto)
+eln_age_gen_clin <- c(eln_age_gen,clin)
+eln_age_gen_demo <- c(eln_age_gen,demo)
+eln_age_gen_demo_without_age <- c(eln_age_gen,demo_without_age)
+
+eln_gen_cyto_clin_demo <- c(eln_gen_cyto,clin,demo)
+
+# USEFUL FOR COMP
+comp_age <- c(comp,age)
+comp_gen <- c(comp,gen)
+comp_cyto <- c(comp,cyto)
+comp_clin <- c(comp,clin)
+comp_demo <- c(comp,demo)
+comp_demo_without_age <- c(comp,demo_without_age)
+comp_gen_cyto <- c(comp_gen,cyto)
+comp_clin_demo <- c(comp_clin,demo)
+comp_gen_cyto_clin_demo <- c(comp_gen_cyto,clin,demo)
+
+#USEFUL FOR GEN
+gen_age <- c(gen,age)
+gen_cyto <- c(gen,cyto)
+gen_clin <- c(gen,clin)
+gen_demo <- c(gen,demo)
+gen_demo_without_age <- c(gen,demo_without_age)
+gen_clin_demo <- c(gen_clin,demo)
+gen_cyto_clin_demo <- c(gen_cyto,clin,demo)
+
+#USEFUL FOR CYTO 
+cyto_age <- c(cyto,age)
+cyto_clin <- c(cyto,clin)
+cyto_demo <- c(cyto,demo)
+gen_demo_without_age <- c(gen,demo_without_age)
+cyto_clin_demo <- c(cyto_clin,demo)
+cyto_gen_demo <- c(gen_cyto,demo)
+
+
+clin_age <-c(clin,age)
+
+
+y <- data.matrix(df_final[,c("os","os_status")])
+
+prognosis_features<- list(eln_age_gen=eln_age_gen,eln_age_cyto=eln_age_cyto,eln_age_clin=eln_age_clin,eln_gen_cyto=eln_gen_cyto,eln_gen_clin=eln_gen_clin,eln_gen_demo=eln_gen_demo,eln_gen_demo_without_age=eln_gen_demo_without_age,
+                          eln_cyto_clin=eln_cyto_clin,eln_cyto_demo=eln_cyto_demo,eln_cyto_demo_without_age=eln_cyto_demo_without_age,eln_clin_demo=eln_clin_demo,eln_clin_demo_without_age=eln_clin_demo_without_age,
+                          eln_age_gen_cyto=eln_age_gen_cyto,eln_age_gen_clin=eln_age_gen_clin,eln_age_gen_demo=eln_age_gen_demo,eln_age_gen_demo_without_age=eln_age_gen_demo_without_age,eln_gen_cyto_clin_demo)
+               
+predictors <- c(rep(list(predictorGLM),6),rep(list(predictorRF),1),predictorBoost,predictorRFX)
+str_predictors <-c(rep("CoxGLM",6),rep("RFS",1),"CoxBoost","RFX")
+l_alpha <-seq(0,1,0.2)
+l_ntree <- seq(1050)
+mc.cores <- 30
+nodesize <- c(20)
+for (i in 1:length(prognosis_features)){
+    print("DONE")
+    x <- data.matrix(df_final[,prognosis_features[[i]]])
+    write.table(launch_prognosis(x=x,y=y,predictors=predictors,str_predictors=str_predictors,l_alpha=l_alpha,nrepeats=5,
+                l_ntree=l_ntree,mc.cores=mc.cores,nodesize=nodesize),paste(names(prognosis_features)[i],".tsv",sep=""),quote=F,sep='\t')
+    print("DONE")
+    }
---
+++ analysis/prognosis/Prognosis_final_July/prognosis.R
@@ -0,0 +1,98 @@
+library(glmnet)
+library(doMC)
+library(survival)
+library(data.table)
+library(mltools)
+library(CoxBoost)
+library(randomForestSRC)
+library(CoxHD)
+source('../tools_prognosis/run_prognosis.R')
+# RFS=105=15*7 for str but only right rep(list(predictorRF),length(l_ntree)) because there is an inside for loop
+
+df_final <- read.table("prognosis_without_comp.tsv",sep='\t',header=T)
+
+### To try when component availables: ###
+
+all_features <-c(1:181) #not used
+clin_demo_comp <-c(154:181) #not used
+clin_demo_cyto_gen_comp <- c(2:181) #not used
+comp <- c(163:181) #not used
+cyto_comp <-c(85:153,163:181) #not used
+cyto_gen_comp <- c(2:153,163:181) #not used
+eln_clin_demo_comp <- c(1,154:181) #not used
+eln_cyto_comp <- c(1,85:153,163:181) #not used
+eln_cyto_gen_comp <- c(1:153,163:181) #not used
+eln_gen_comp <- c(1:84,163:181) #not used
+gen_comp <- c(2:84,163:181) #not used
+clin_comp <- c(153:160,163:181) #not used
+clin_cyto_comp <- c(85:160,163:181) #not used
+clin_gen_comp <- c(2:84,154:160,163:181) #not used
+eln_clin_comp <- c(1,154:160,163:181) #not used
+
+#Without age
+all_features_without_age <-c(1:161,163:181) #not used
+clin_demo_comp_without_age <-c(154:161,163:181) #not used
+clin_demo_cyto_gen_comp_without_age <- c(2:161,163:181) #not used
+eln_clin_demo_comp_without_age <- c(1,154:161,163:181) #not used
+
+
+### To try now: ###
+
+###With age:
+eln_clin_gen <-  c(1:84,154:160)
+eln_demo_gen <- c(1:84,161:162)
+eln_clin_demo_cyto_gen <- c(1:162)
+eln_clin_demo_cyto <- c(1,85:162)
+eln_clin_demo_gen <- c(1:84,154:162)
+eln_clin_demo <- c(1,154:162)
+eln_clin <- c(1,154:160)
+eln_cyto_gen <- c(1:153)
+clin_demo_cyto_gen <- c(2:162)
+clin_demo_cyto <- c(85:162)
+clin_demo_gen <- c(2:84,154:162)
+clin_demo <- c(154:162)
+cyto_gen <- c(2:153)
+cyto <- c(85:153)
+gen <- c(2:84)
+clin_gen <- c(2:84,154:160)
+clin_cyto <- c(85:160)
+demo_gen <- c(2:84,161:162)
+demo_cyto <- c(85,153,161:162)   #PROBLEMMMM c(85:153,161:162)
+
+###Without age:
+eln_demo_gen_without_age <- c(1:84,161)
+eln_clin_demo_cyto_gen_without_age <- c(1:161)
+eln_clin_demo_cyto_without_age <- c(1,85:161)
+eln_clin_demo_gen_without_age <- c(1:84,154:161)
+eln_clin_demo_without_age <- c(1,154:161)
+clin_demo_cyto_gen_without_age <- c(2:161)
+clin_demo_cyto_without_age <- c(85:161)
+clin_demo_gen_without_age <- c(2:84,154:161)
+clin_demo_without_age <- c(154:161)
+demo_gen_without_age <- c(2:84,161)
+demo_cyto_without_age <- c(85,153,161)   #PROBLEMMMM c(85:153,161:162)
+
+
+y <- data.matrix(df_final[,c("os","os_status")])
+
+prognosis_features<- list(eln_clin_gen=eln_clin_gen,eln_demo_gen=eln_demo_gen,eln_clin_demo_cyto_gen=eln_clin_demo_cyto_gen,eln_clin_demo_cyto=eln_clin_demo_cyto,
+                          eln_clin_demo_gen=eln_clin_demo_gen,eln_clin_demo=eln_clin_demo,eln_clin=eln_clin,eln_cyto_gen=eln_cyto_gen,clin_demo_cyto_gen=clin_demo_cyto_gen,
+                          clin_demo_cyto=clin_demo_cyto,clin_demo_gen=clin_demo_gen,clin_demo=clin_demo,cyto_gen=cyto_gen,cyto=cyto,gen=gen,clin_gen=clin_gen,clin_cyto=clin_cyto,
+                          demo_gen=demo_gen,demo_cyto=demo_cyto,eln_demo_gen_without_age=eln_demo_gen_without_age,eln_clin_demo_cyto_gen_without_age=eln_clin_demo_cyto_gen_without_age,
+                          eln_clin_demo_cyto_without_age=eln_clin_demo_cyto_without_age,eln_clin_demo_gen_without_age=eln_clin_demo_gen_without_age,
+                          eln_clin_demo_without_age=eln_clin_demo_without_age,clin_demo_cyto_gen_without_age=clin_demo_cyto_gen_without_age,clin_demo_cyto_without_age=clin_demo_cyto_without_age,
+                          clin_demo_gen_without_age=clin_demo_gen_without_age,clin_demo_without_age=clin_demo_without_age,demo_gen_without_age=demo_gen_without_age,demo_cyto_without_age=demo_cyto_without_age)
+
+predictors <- c(rep(list(predictorGLM),11),rep(list(predictorRF),7),predictorBoost,predictorRFX)
+str_predictors <-c(rep("CoxGLM",11),rep("RFS",28),"CoxBoost","RFX")
+l_alpha <-seq(0,1,0.1)
+l_ntree <- seq(300,1200,150)
+mc.cores <- 8
+nodesize <- c(5,10,20,30)
+for (i in 1:length(prognosis_features)){
+    print("DONE")
+    x <- data.matrix(df_final[,prognosis_features[[i]]])
+    write.table(launch_prognosis(x=x,y=y,predictors=predictors,str_predictors=str_predictors,l_alpha=l_alpha,nrepeats=2,
+                l_ntree=l_ntree,mc.cores=mc.cores,nodesize=nodesize),paste(names(prognosis_features)[i],".tsv",sep=""),quote=F,sep='\t')
+    print("DONE")
+    }
